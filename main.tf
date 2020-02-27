23340cb1f01d08e0f0ffe0ba695a4d5f73e1735f
Daniel Betz <daniel.betz@jambit.com>
2020-02-27 15:25:19 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "hcloud" {
+  token = var.HCLOUD_TOKEN
+}
+
+resource "hcloud_network" "kubernetes" {
+  name = "kubenet"
+  ip_range = var.network_range
+}
+
+resource "hcloud_network_subnet" "kubernetes" {
+  network_id = hcloud_network.kubernetes.id
+  type = "server"
+  network_zone = var.network_zone
+  ip_range = var.network_subnet
+}
+
+resource "hcloud_floating_ip" "kubernetes" {
+  type = "ipv4"
+  home_location = var.location
+  description = "Kubernetes Loadbalancer IP"
+  name = "kubernetes-loadbalancer"
+}
+
+resource "hcloud_ssh_key" "kubernetes_localfile" {
+  count = var.ssh_rsa_key != "" ? 0 : 1
+  name = var.ssh_key_name
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "hcloud_ssh_key" "kubernetes_fromvar" {
+  count = var.ssh_rsa_key != "" ? 1 : 0
+  name = var.ssh_key_name
+  public_key = var.ssh_rsa_key
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "HCLOUD_TOKEN" {}
+variable "kubernetes_prefix" {}
+variable "location" {}
+variable "network_range" {}
+variable "network_subnet" {}
+variable "network_zone" {}
+variable "ssh_key_name" {}
+variable "ssh_rsa_key" {}
